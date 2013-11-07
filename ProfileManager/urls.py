17a7eb3425ea9af725f653bffd0093a13e7fe3d7
yboiko105 <yboiko105@gmail.com>
2013-11-07 08:21:41 +0200

    URL support update

--- ProfileManager/urls.py
+++ ProfileManager/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,3 +20,7 @@ urlpatterns = patterns('',
 
     url(r'', include('social_auth.urls')),
 )
+
+if settings.DEBUG == True:
+    urlpatterns += patterns('',
+                            url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
